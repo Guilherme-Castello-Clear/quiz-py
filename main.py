eef3ded2e0e64dcb68a5fd2b3a0aa689d8e5cfab
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-10-26 17:04:43 -0300

    Feat: Creating new quests

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-class User:
-    def __init__(self, name):
-        self.name = name
+from question_model import Question
+from data import question_data
 
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-user_1 = User("Castellão")
-print(user_1.name)
+print(question_bank)
